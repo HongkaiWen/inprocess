d7499505607550d6d7000aaed7d9b1aef3393313
hongkai <java_win@yeah.net>
2013-04-19 22:07:59 +0800

    encoding filter to avoid @#$%

--- servlet/src/main/java/com/xiaokai/servlet/action/EncodingFilter.java
+++ servlet/src/main/java/com/xiaokai/servlet/action/EncodingFilter.java
@@ -9,20 +9,27 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
-public class CommonFilter implements Filter {
+/**
+ * set utf-8 
+ * 
+ * @author Administrator
+ */
+public class EncodingFilter implements Filter {
+	
+	private static final String ENCODING = "UTF-8";
 
 	public void init(FilterConfig filterConfig) throws ServletException {
-		
+		// do nothing
 	}
 
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
-		System.out.println("filter");
+		response.setCharacterEncoding(ENCODING);
+		request.setCharacterEncoding(ENCODING); 
 		chain.doFilter(request, response);
 	}
 
 	public void destroy() {
-		
+		// do nothing
 	}
-
 }
