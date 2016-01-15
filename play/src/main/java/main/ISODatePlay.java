8304d0faa261f76ad5ef2701b0e72a7a7e9a6f36
hongkai <446410466@qq.com>
2016-01-15 13:26:31 +0800

    iso date play

---
+++ play/src/main/java/main/ISODatePlay.java
@@ -0,0 +1,17 @@
+package main;
+
+import org.joda.time.DateTime;
+
+import java.util.Date;
+
+/**
+ * Created by hongkai on 2016/1/15.
+ */
+public class ISODatePlay {
+    public static void main(String args[]){
+        //ISO format string to java.util.Date
+        String dateStr = "2015-12-25T10:12:15.078581168Z";
+        Date date = new DateTime(dateStr).toDate();
+        System.out.println(date);
+    }
+}
