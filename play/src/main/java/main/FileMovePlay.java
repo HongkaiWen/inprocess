3449dc418cfde742a7cb7d3711fcb867d07b4847
hongkai <446410466@qq.com>
2016-01-27 17:28:54 +0800

    file move play

--- play/src/main/java/main/FileMovePlay.java
+++ play/src/main/java/main/FileMovePlay.java
@@ -9,8 +9,7 @@ import java.nio.file.*;
 public class FileMovePlay {
 
     public static void main(String args[]) throws IOException {
-        Path source = FileSystems.getDefault().getPath("d:/temp/temp.7z.bak");
-        Path target = FileSystems.getDefault().getPath("d:/temp/temp.7z");
-        Files.move(source, target, StandardCopyOption.REPLACE_EXISTING);
+        Path source = FileSystems.getDefault().getPath("d:/temp/temp.7z");
+        Files.move(source, source.resolveSibling("d:/temp/temp.7z.bak"), StandardCopyOption.REPLACE_EXISTING);
     }
 }
