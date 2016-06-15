a7d4f8558b5020020ca62fbf2fc74a08cc6db882
hongkai <446410466@qq.com>
2016-06-15 13:05:58 +0800

    spring aspect play

--- play/src/main/java/aspect/AspectPlay.java
+++ play/src/main/java/aspect/AspectPlay.java
@@ -4,6 +4,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
+ * ref to
+ * http://www.idanfridman.com/clean-auditing-infrastructure-for-your-app-using-aop-custom-annotations-and-reflection/
+ * http://www.mkyong.com/spring3/spring-aop-aspectj-annotation-example/
  * Created by hongkai on 2016/6/15.
  */
 public class AspectPlay {
