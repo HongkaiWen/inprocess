5a7ec87373cc8220664293121d9e77b1c87e246e
hongkai <446410466@qq.com>
2017-08-24 10:33:04 +0800

    play

--- play/src/main/java/des/ThreeDESUtil.java
+++ play/src/main/java/des/ThreeDESUtil.java
@@ -12,47 +12,55 @@ import javax.crypto.spec.DESedeKeySpec;
 import javax.crypto.spec.IvParameterSpec;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
 
 /**
  * http://www.blogjava.net/amigoxie/archive/2014/07/06/415503.html
  */
 public class ThreeDESUtil {
-    // 算法名称 
-    public static final String KEY_ALGORITHM = "desede";
-    // 算法名称/加密模式/填充方式 
-    public static final String CIPHER_ALGORITHM = "desede/CBC/NoPadding";
 
     /** 
      * CBC加密 
      * @param key 密钥 
-     * @param keyiv IV 
-     * @param data 明文 
+     * @param data 明文
      * @return Base64编码的密文 
      * @throws Exception 
      */
-    public static byte[] des3EncodeCBC(byte[] key, byte[] keyiv, byte[] data) throws Exception {
+    public static String des3EncodeCBC(String key, String data) throws Exception {
         Security.addProvider(new BouncyCastleProvider()); 
-        Key deskey = keyGenerator(new String(key));
+        Key deskey = keyGenerator(key);
         Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
-        IvParameterSpec ips = new IvParameterSpec(keyiv);
+        IvParameterSpec ips = new IvParameterSpec(KEY_IV);
         cipher.init(Cipher.ENCRYPT_MODE, deskey, ips);
-        byte[] bOut = cipher.doFinal(data);
-        for (int k = 0; k < bOut.length; k++) {
-            System.out.print(bOut[k] + " ");
-        }
-        System.out.println("");
-        return bOut;
+        byte[] bOut = cipher.doFinal(data.getBytes(ENCODING));
+        return new BASE64Encoder().encode(bOut);
     }
 
     /** 
-     *   
-     * 生成密钥key对象 
-     * @param KeyStr 密钥字符串 
-     * @return 密钥对象 
-     * @throws InvalidKeyException   
-     * @throws NoSuchAlgorithmException   
-     * @throws InvalidKeySpecException   
+     * CBC解密 
+     * @param key 密钥 
+     * @param data Base64编码的密文
+     * @return 明文 
      * @throws Exception 
+     */
+    public static String des3DecodeCBC(String key, String data) throws Exception {
+        Key deskey = keyGenerator(key);
+        Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
+        IvParameterSpec ips = new IvParameterSpec(KEY_IV);
+        cipher.init(Cipher.DECRYPT_MODE, deskey, ips);
+        byte[] bOut = cipher.doFinal(new BASE64Decoder().decodeBuffer(data));
+        return new String(bOut, ENCODING);
+    }
+
+    /**
+     *
+     * 生成密钥key对象
+     * @return 密钥对象
+     * @throws InvalidKeyException
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeySpecException
+     * @throws Exception
      */
     private static Key keyGenerator(String keyStr) throws Exception {
         byte input[] = HexString2Bytes(keyStr);
@@ -66,9 +74,9 @@ public class ThreeDESUtil {
         if (c >= 'A') return (c - 'A' + 10) & 0x0f;
         return (c - '0') & 0x0f;
     }
- 
-    // 从十六进制字符串到字节数组转换 
-    public static byte[] HexString2Bytes(String hexstr) {
+
+    // 从十六进制字符串到字节数组转换
+    private static byte[] HexString2Bytes(String hexstr) {
         byte[] b = new byte[hexstr.length() / 2];
         int j = 0;
         for (int i = 0; i < b.length; i++) {
@@ -79,33 +87,22 @@ public class ThreeDESUtil {
         return b;
     }
 
-    /** 
-     * CBC解密 
-     * @param key 密钥 
-     * @param keyiv IV 
-     * @param data Base64编码的密文 
-     * @return 明文 
-     * @throws Exception 
-     */
-    public static byte[] des3DecodeCBC(byte[] key, byte[] keyiv, byte[] data) throws Exception {
-        Key deskey = keyGenerator(new String(key));
-        Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
-        IvParameterSpec ips = new IvParameterSpec(keyiv);
-        cipher.init(Cipher.DECRYPT_MODE, deskey, ips);
-        byte[] bOut = cipher.doFinal(data);
-        return bOut;
-    }
+    // 算法名称
+    private static final String KEY_ALGORITHM = "desede";
+    // 算法名称/加密模式/填充方式
+    private static final String CIPHER_ALGORITHM = "desede/CBC/NoPadding";
 
-    public static void main(String[] args) throws Exception {
-        byte[] key = "6C4E60E55552386C759569836DC0F83869836DC0F838C0F7".getBytes();
-        byte[] keyiv = { 1, 2, 3, 4, 5, 6, 7, 8 };
-        byte[] data = "amigoxieamigoxie        ".getBytes("UTF-8");
-        System.out.println("data.length=" + data.length);
-        System.out.println("CBC加密解密");
-        byte[] str5 = des3EncodeCBC(key, keyiv, data);
-        System.out.println(new sun.misc.BASE64Encoder().encode(str5));
+    private static final byte[] KEY_IV = { 1, 2, 3, 4, 5, 6, 7, 8 };
 
-        byte[] str6 = des3DecodeCBC(key, keyiv, str5);
-        System.out.println(new String(str6, "UTF-8"));
-    }
+    private static final String ENCODING = "utf-8";
+
+//    public static void main(String[] args) throws Exception {
+//       String key = "6C4E60E55552386C759569836DC0F83869836DC0F838C0F7";
+//        String data = "amigoxieamigoxie        ";
+//        String str5 = des3EncodeCBC(key, data);
+//        System.out.println(str5);
+//
+//        String str6 = des3DecodeCBC(key, str5);
+//        System.out.println(str6);
+//    }
 }
